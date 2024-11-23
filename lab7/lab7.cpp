5c8fe225398b2b51f611dd5fd84352500e22c0b4
LAlinaV <ladik151104@gmail.com>
2024-11-23 15:35:38 +0300

    main

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -1,20 +1,58 @@
-﻿// lab7.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
+#include "memory.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    const size_t total_pages = 1000;
+    MemoryManager manager(total_pages);
+
+    // Эксперимент 1: Время выделения памяти
+    std::vector<double> allocation_times;
+    for (size_t i = 0; i < total_pages; i++) {
+        double time = measure_time([&]() { manager.allocate_memory(); });
+        allocation_times.push_back(time);
+    }
+
+    // Эксперимент 2: Время освобождения памяти
+    std::vector<double> deallocation_times;
+    for (size_t i = 0; i < total_pages; i++) {
+        void* address = manager.allocate_memory();
+        double time = measure_time([&]() { manager.free_memory(address); });
+        deallocation_times.push_back(time);
+    }
+
+    // Эксперимент 3: Время чтения и записи данных
+    std::vector<double> read_write_times;
+    for (size_t size = 1; size <= PAGE_SIZE; size *= 2) {
+        void* address = manager.allocate_memory();
+        char data[PAGE_SIZE];
+        memset(data, 'a', size);
+
+        double write_time = measure_time([&]() { manager.write_memory(address, data, size); });
+        double read_time = measure_time([&]() { manager.read_memory(address, data, size); });
+
+        read_write_times.push_back(write_time + read_time);
+        manager.free_memory(address);
+    }
+
+    // Вывод результатов
+    std::cout << "Allocation Times:" << std::endl;
+    for (const auto& time : allocation_times) {
+        std::cout << time << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Deallocation Times:" << std::endl;
+    for (const auto& time : deallocation_times) {
+        std::cout << time << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Read/Write Times:" << std::endl;
+    for (const auto& time : read_write_times) {
+        std::cout << time << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
